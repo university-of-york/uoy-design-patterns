88940de9c5b3dee1195a528e98cbc56dfef4730c
Chris Marsh <chris.marsh@york.ac.uk>
2015-09-11 09:27:17 +0100

    Equal height rows module started

---
+++ src/js/app/equal-height-row.js
@@ -0,0 +1,19 @@
+/*
+
+---
+title: Equal height rows
+name: equal-height-row-module
+category: Javascript
+---
+
+ */
+define(['jquery', 'app/utils'], function ($, UTILS) {
+
+  var EQUALHEIGHT = function (options) {
+    console.log(options)
+    return true;
+  };
+
+  return EQUALHEIGHT;
+
+});
