7fe3ac1f82377bf63caa23715907bc9bdead7a25
Chris Marsh <chris.marsh@york.ac.uk>
2015-09-16 11:38:55 +0100

    Basic equal height row JS added. Component mixin added and applied to base component units.

--- src/js/app/equal-height-row.js
+++ src/js/app/equal-height-row.js
@@ -10,8 +10,28 @@ category: Javascript
 define(['jquery', 'app/utils'], function ($, UTILS) {
 
   var EQUALHEIGHT = function (options) {
-    console.log(options)
+    if (!options.row) return false;
+    this.row = options.row;
+    this.boxes = this.row.children('.o-grid__box');
+    this.maxHeight = 0;
+    this.getMaxHeight(this.setMaxHeight);
     return true;
+  };
+
+  EQUALHEIGHT.prototype.getMaxHeight = function(onComplete) {
+    var that = this,
+        boxCount = this.boxes.length;
+    $.each(this.boxes, function(i, box) {
+      that.maxHeight = Math.max(that.maxHeight, $(box).outerHeight());
+      if (i === boxCount - 1) {
+        onComplete.apply(that);
+      }
+    });
+
+  };
+
+  EQUALHEIGHT.prototype.setMaxHeight = function() {
+    this.boxes.height(this.maxHeight);
   };
 
   return EQUALHEIGHT;
