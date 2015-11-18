900c9665627c624e71c60fca120cee609b2638f3
Chris Marsh <chris.marsh@epiphanysolutions.co.uk>
2015-11-18 12:00:12 +0000

    Added new fonts

---
+++ src/js/fonts.js
@@ -0,0 +1,20 @@
+(function() {
+    var path = '//easy.myfonts.net/v2/js?sid=276131(font-family=Inka+B+Text+Bold)&sid=276148(font-family=Inka+A+Small+Bold)&sid=276151(font-family=Inka+A+Small+Medium)&sid=276153(font-family=Inka+A+Small+Regular)&key=UqXmoPTHgy',
+        protocol = ('https:' == document.location.protocol ? 'https:' : 'http:'),
+        trial = document.createElement('script');
+    trial.type = 'text/javascript';
+    trial.async = true;
+    trial.src = protocol + path;
+    var head = document.getElementsByTagName("head")[0];
+    head.appendChild(trial);
+})();
+(function() {
+    var path = '//easy.myfonts.net/v2/js?sid=276396(font-family=Monsal+Gothic+Bold)&sid=276397(font-family=Monsal+Gothic+Book)&sid=276400(font-family=Monsal+Gothic+Light)&sid=276404(font-family=Monsal+Gothic+Medium)&key=EeDfZLlqil',
+        protocol = ('https:' == document.location.protocol ? 'https:' : 'http:'),
+        trial = document.createElement('script');
+    trial.type = 'text/javascript';
+    trial.async = true;
+    trial.src = protocol + path;
+    var head = document.getElementsByTagName("head")[0];
+    head.appendChild(trial);
+})();
