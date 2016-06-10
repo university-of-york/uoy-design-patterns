31522333cd5b9f6023086883e0bc589e8929a615
Chris Marsh <chris.marsh@york.ac.uk>
2016-06-10 14:48:22 +0100

    Sorting out separate formstack styles

--- config/scopedCSS.js
+++ config/scopedCSS.js
@@ -2,18 +2,18 @@ module.exports = {
   tinymce: {
     srcPath: 'src/sass/scopes/_tinymce.scss',
     buildPath: 'build/css/tinymce.css'
-  },
-  formstack: {
-    srcPath: 'src/sass/scopes/_formstack.scss',
-    buildPath: 'build/css/formstack.css'
-  },
-  formstackDev: {
-    srcPath: 'src/sass/scopes/_formstack.scss',
-    buildPath: 'dev/css/formstack.css'
-  },
-  test: {
-    srcPath: 'src/sass/scopes/_test.scss',
-    buildPath: 'dev/css/test.css',
-    includeExtends: true
+  // },
+  // formstack: {
+  //   srcPath: 'src/sass/scopes/formstack.scss',
+  //   buildPath: 'build/css/formstack.css'
+  // },
+  // formstackDev: {
+  //   srcPath: 'src/sass/scopes/formstack.scss',
+  //   buildPath: 'dev/css/formstack.css'
+  // },
+  // test: {
+  //   srcPath: 'src/sass/scopes/test.scss',
+  //   buildPath: 'dev/css/test.css',
+  //   includeExtends: true
   }
 };
--- gruntfile.js
+++ gruntfile.js
@@ -20,7 +20,7 @@ module.exports = function (grunt) {
   // Local development
   grunt.registerTask('dev', ['makedocs:dev', 'copy:dev', 'copy:templates', 'jshint:dev', 'compass:dev', 'postcss:dev', 'browserSync:dev', 'watch', 'clean:dev']);
   // Build process - minified CSS and JS
-  grunt.registerTask('build', ['bump:patch', 'clean:build', 'makedocs:build', 'copy:build', 'compass:build', 'postcss:build', 'scopedCSS', 'clean:precssmin', 'modernizr', 'cssmin', 'newer:imagemin', 'requirejs', 'jshint:dev', 'header:build', 'clean:postbuild']);
+  grunt.registerTask('build', ['clean:build', 'makedocs:build', 'copy:build', 'compass:build', 'postcss:build', 'scopedCSS', 'clean:precssmin', 'modernizr', 'cssmin', 'newer:imagemin', 'requirejs', 'jshint:dev', 'header:build', 'clean:postbuild']);
   // Publish to york.ac.uk/pattern-library
   grunt.registerTask('publish', ['build', 'sftp:build']);
   //grunt.registerTask('live', ['build', 'bump:major', 'header:live', 'clean:live']);
