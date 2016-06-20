3908e1483338ee958a433a6255723e5a34cd09ff
Chris Marsh <chris.marsh@york.ac.uk>
2016-06-20 16:30:55 +0100

    Updated grunt modernizr. You should run  after pulling this commit.

--- config/modernizr.js
+++ config/modernizr.js
@@ -1,7 +1,7 @@
 module.exports = {
   build: {
     devFile : "src/js/vendor/modernizr/modernizr.js",
-    outputFile : "build/js/modernizr.min.js",
+    dest : "build/js/modernizr.min.js",
     files : {
       src: ['src/js/app/**/*.js', 'src/sass/**/*']
      }
