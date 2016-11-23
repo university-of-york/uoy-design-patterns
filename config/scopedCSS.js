dd5ee19d50aa74c82744931bcec7059ff8b19a82
Chris Marsh <chris.marsh@york.ac.uk>
2016-11-23 12:32:10 +0000

    Integrated Formstack into usual flow (no imports)

--- config/scopedCSS.js
+++ config/scopedCSS.js
@@ -3,18 +3,5 @@ module.exports = {
     srcPath: 'src/sass/scopes/_tinymce.scss',
     buildPath: 'build/css/tinymce.css',
     includeExtends: true
-  // },
-  // formstack: {
-  //   srcPath: 'src/sass/scopes/formstack.scss',
-  //   buildPath: 'build/css/formstack.css'
-  // },
-  // formstackDev: {
-  //   srcPath: 'src/sass/scopes/formstack.scss',
-  //   buildPath: 'dev/css/formstack.css'
-  // },
-  // test: {
-  //   srcPath: 'src/sass/scopes/test.scss',
-  //   buildPath: 'dev/css/test.css',
-  //   includeExtends: true
   }
 };
