315867c1c40014f6aaec05a733395ecb601a952c
Rob Kendal <38687284+rpk506@users.noreply.github.com>
2018-07-10 13:28:36 +0100

    Update static asset paths to stable location

--- src/js/app/utils.js
+++ src/js/app/utils.js
@@ -102,7 +102,7 @@
     // Fixes logo where SVG isn't supported
     fixLogo: function() {
       if (Modernizr.svg) return;
-      $('.c-main-header__logo').attr('src', 'https://www.york.ac.uk/static/1.4/img/logo.png');
+      $('.c-main-header__logo').attr('src', 'https://www.york.ac.uk/static/stable/img/logo.png');
     },
 
     // Fixes figures where the content is overspilling (figures)
