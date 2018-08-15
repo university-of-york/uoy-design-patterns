e74316bf1020328fe0fd6560252f09c411434fa7
Rob Kendal <38687284+rpk506@users.noreply.github.com>
2018-08-15 11:50:43 +0100

    add initial scaffolded files

---
+++ src/js/app/data-firebase.js
@@ -0,0 +1,30 @@
+/*
+
+---
+title: Firebase Data Access Module
+name: data-firebase
+category: Javascript
+---
+
+ */
+define(['jquery'], function ($) {
+
+    var DATA_FIREBASE = (function(){
+
+        // Variables
+
+        // Private functions
+
+        // Public functions
+        var init = function() {
+
+        };
+
+        return {
+            init: init
+        };
+    }());
+
+
+    return DATA_FIREBASE;
+});
