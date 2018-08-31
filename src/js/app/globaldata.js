c7096997245c6367bdfe63e84aea4411d2b6c87e
Rob Kendal <38687284+rpk506@users.noreply.github.com>
2018-08-31 10:56:06 +0100

    change clearing call message

--- src/js/app/globaldata.js
+++ src/js/app/globaldata.js
@@ -4,7 +4,7 @@ define(['jquery'], function ($) {
             noCourseMessage: "<p>There are no vacancies in this department for September 2018. <a href=\"//www.york.ac.uk/study/undergraduate/courses/all\">Explore your options for 2019 entry.</a></p>",
             docID: "1w8XM3NZeYThYpfYBLfEiJBj7SbA2xL-JABHnDW_w",
             backupDoc: "http://www.york.ac.uk/static/data/clearing/2018.json",
-            callToApplyText: "Our clearing hotline will open at 8am on 16 August"
+            callToApplyText: "Call Admissions on <a href='tel:+441904234000'>01904 234000</a>"
         },
         firebaseAPISettings: {
             firebaseScriptAll: "https://www.gstatic.com/firebasejs/5.3.0/firebase.js",
