c3b4088b52124be97422066487b152ca45b4d8f7
Dave Hill <dave.hill@york.ac.uk>
2019-08-15 12:12:33 +0100

    Remove apply button for 2019 courses

--- src/js/app/clearing-table.js
+++ src/js/app/clearing-table.js
@@ -1049,7 +1049,7 @@ define(['jquery', 'app/searchables', 'app/utils', 'app/modal-link'],
   };
 
  // Remove apply button from 2019 course overview
- if(window.location.href.indexOf("courses") > -1)
+ if(window.location.href.indexOf("courses-2019") > -1)
 {
   $("#btnApplyForCourse").parent( "p" ).parent( "div" ).remove();
 }
