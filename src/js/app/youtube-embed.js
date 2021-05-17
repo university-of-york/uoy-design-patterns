2134f5be018171676b79e659181ba6086025af18
Nick Pearson <nick.pearson@york.ac.uk>
2021-05-17 16:02:34 +0100

    Cleans up Youtube embed code

--- src/js/app/youtube-embed.js
+++ src/js/app/youtube-embed.js
@@ -59,57 +59,15 @@ define(['jquery', 'app/utils'], function ($, UTILS) {
 
         this.iframe = this.createIframe();
 
-        // var that = this;
-        // var resizeFn = UTILS.debounce(function (e) {
-        //     that.setDimensions();
-        // }, 250);
-
-        // $window.on('resize', null, {that: this}, resizeFn);
-
         // console.info(this);
 
     };
 
-    // YOUTUBE.prototype.getDimensions = function () {
-    //     var videoWidth = this.container.width();
-    //     var videoHeight = Math.floor((videoWidth / 16) * 9);
-    //     return {
-    //         width: videoWidth,
-    //         height: videoHeight
-    //     };
-    // };
-
-    // YOUTUBE.prototype.setDimensions = function () {
-
-    //     // Check to see if it's a fullscreen resize
-    //     var screenW = screen.width;
-    //     var screenH = screen.height;
-    //     var windowW = $window.width();
-    //     var windowH = $window.height();
-    //     var isFullscreen = (screenW == windowW) && (screenH == windowH);
-
-    //     if (isFullscreen === true) return false;
-
-    //     var videoDimensions = this.getDimensions();
-
-    //     this.iframe.attr({
-    //         width: videoDimensions.width,
-    //         height: videoDimensions.height
-    //     });
-
-    //     // Fire update event
-    //     $window.trigger('content.updated', ['youtube', this]);
-    // };
-
-
     YOUTUBE.prototype.createIframe = function () {
-
-        // var videoDimensions = this.getDimensions();
 
         // create the embed code
         var iframe = $('<iframe>').attr({
-            // width: videoDimensions.width,
-            // height: videoDimensions.height,
+
             src: this.url,
             allowfullscreen: true
         });
